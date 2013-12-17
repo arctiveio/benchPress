d59c2ffd8081a8988dc7479ff7f7fc64bf4945cc
Piyush Verma <github@piyushverma.net>
2013-12-17 09:37:35 +0530

    test_uploader checks for a bunch of other functionalities

--- simtools/test_uploader.py
+++ simtools/test_uploader.py
@@ -1,17 +1,21 @@
 import unittest
 
-from simtools.uploader.external_tools import validate_url
 from simtools.uploader import UploaderException
+from simtools.uploader.external_tools import validate_url
+from simtools.uploader.external_tools import guess_content_type
 
 class TestToken(unittest.TestCase):
     valid_video = "http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4"
     invalid_video = "http://google.com"
     non_existent_url = "http://www.promod.jhg/"
 
+    tmp_path = "/tmp/hello.txt"
+    tmp_content_type = "text/plain"
+
     def test_404_video(self):
         with self.assertRaises(IOError):
             validate_url(self.non_existent_url, "video")
-    
+
     def test_invalid_video(self):
         with self.assertRaises(UploaderException):
             validate_url(self.invalid_video, "video")
@@ -26,14 +30,23 @@ class TestToken(unittest.TestCase):
         with self.assertRaises(UploaderException):
             validate_url(self.valid_video, maxsize=5510000)
 
-    #def test_month_long_expiry(self):
-    #    pass
+    def test_guess_content_type_filebody(self):
+        temp_file = open(self.tmp_path, "w")
+        temp_file.writelines(["Hello", "World"])
+        temp_file.close()
 
-    #def test_expired_token(self):
-    #    pass
+        temp_file = open(self.tmp_path, "r")
+        self.assertEquals(guess_content_type(temp_file), self.tmp_content_type)
+        temp_file.close()
 
-    #def test_valid_token(self):
-    #    pass
+    def test_guess_content_type_filepath(self):
+        temp_file = open(self.tmp_path, "w")
+        temp_file.writelines(["Hello", "World"])
+        temp_file.close()
+
+        self.assertEquals(
+            guess_content_type(self.tmp_path),
+            self.tmp_content_type)
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
