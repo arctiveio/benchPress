84642d6cd1d97eafed1ec68a2db35782132f24f7
Piyush Verma <github@piyushverma.net>
2014-06-11 15:29:33 +0530

    Add signature decorator to check CLI args

--- api/core/decorators.py
+++ api/core/decorators.py
@@ -7,3 +7,15 @@ def authorize(email, password):
             return x
         return inner
     return _check
+
+def signature(params):
+    def _check(func):
+        def inner(self, *args, **kwargs):
+            for key in params:
+                if not getattr(self.cli_args, key, None):
+                    self.logger.error("Test need Command line argument --%s to run" % key)
+                    return
+
+            return func(self, *args, **kwargs)
+        return inner
+    return _check
