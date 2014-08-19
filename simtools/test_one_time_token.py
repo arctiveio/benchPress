a4c02ee4ed0526e109e45dda4e6652a09bb1c758
Amit Sethi <amit.pureenergy@gmail.com>
2014-08-19 16:31:48 +0530

    corrected test for one time token

--- simtools/test_one_time_token.py
+++ simtools/test_one_time_token.py
@@ -2,8 +2,6 @@ import unittest
 
 from simtools.one_time_token import generate_token, validate_token
 
-from simtools.one_time_token import today, get_offset
-from simtools.school import make_md5_password
 
 class TestToken(unittest.TestCase):
     salt = "user_id"
@@ -18,7 +16,7 @@ class TestToken(unittest.TestCase):
 
     def test_expired_token(self):
         validity = -1
-        token = make_md5_password(today()+get_offset(validity), self.salt)
+        token = generate_token(self.salt, validity)
         self.assertFalse(validate_token(token, self.salt, validity=5))
 
     def test_valid_token(self):
